e4be92c9ac2fb289b5d10d31062d4682096d5308
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-02-06 11:35:33 +0100

    first commit with basic structure and database

---
+++ src/main/java/com/svalero/komorebiApi/KomorebiApiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.komorebiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomorebiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomorebiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/komorebiApi/KomorebiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.komorebiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomorebiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
