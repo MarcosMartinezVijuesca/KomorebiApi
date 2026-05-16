1380ffe852cda674637cff66a5166a25ea3cc61b
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-05-16 10:31:47 +0200

    add endpoint total users

--- src/main/java/com/svalero/komorebiApi/config/SecurityConfig.java
+++ src/main/java/com/svalero/komorebiApi/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/schools/*/projects").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT,  "/projects/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/projects/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
